2478315baf7b47abca6d1d5d55e40eef7d8a6c96
XinyuQu <47442607+XinyuQu@users.noreply.github.com>
2019-06-06 13:01:51 -0400

    Create weather.h

---
+++ my_code/weather.h
@@ -0,0 +1,45 @@
+#ifndef WEATHER_H
+#define WEATHER_H
+
+#include <string>
+
+struct GPS {
+    double latitude;
+    double longitude;
+    GPS(double la, double lo) : latitude(la),
+    longitude(lo) {}
+};
+
+std::ostream& operator<<(std::ostream& os, const GPS& gps);
+
+class Date{
+friend std::ostream& operator<<(std::ostream& os, const Date& date);
+public:
+    Date(int d, int m, int );
+private:
+    int day;
+    int month;
+    int year;
+};
+const int UNRATED = -1;
+const int BAD = 0;
+const int OK = 1;
+const int GOOD = 2;
+
+class Weather {
+// << should output name, rating, and use the GPS << to output my_loc
+
+    friend std::ostream& operator<<(std::ostream& os, const Weather& w);
+public:
+    Weather(std::string nm, GPS loc, int rating);
+    std::string get_name() const;
+    int get_rating() const;
+    void set_rating(int new_rating);
+private:
+    std::string station_nm;
+    GPS my_loc;
+    int rating = UNRATED;
+};
+
+
+#endif
