8c0034394832500abf93856b3b1db219b13d1af4
Xinyu_Qu <quxinyuu@hotmail.com>
2019-06-12 12:56:50 -0400

    update

--- my_code/weather.cpp
+++ my_code/weather.cpp
@@ -75,3 +75,70 @@ const double C_to_F = 9 / 5;
 double WReading::get_tempF(){
     return temperature * C_to_F + 32;
 }
+
+
+
+
+// Image (copy path)
+
+
+Image::Image(int w, int h, std::string flnm)
+: width(w), height(h)
+{
+    filename = flnm;
+    image_buf = new char[image_sz()];
+}
+
+// copy constructor:
+Image::Image(const Image& img2) {
+    height = img2.height;
+    width = img2.width;
+    filename = img2.filename;
+    image_buf = new char[image_sz()];
+    for(int i = 0; i < image_sz(); i ++){
+        image_buf[i] = img2.image_buf[i];
+    }
+}
+
+// destructor
+Image::~Image() {
+    if(image_buf != nullptr){
+        delete image_buf;
+    }
+}
+
+// assignment operator:
+Image& Image::operator=(const Image& img2) {
+    if (&img2 != this) {
+        if(image_buf != nullptr){
+            delete image_buf;
+        }
+        height = img2.height;
+        width = img2.width;
+        filename = img2.filename;
+        image_buf = new char[image_sz()];
+        for(int i = 0; i < image_sz(); i ++){
+            image_buf[i] = img2.image_buf[i];
+        }
+    }
+    return *this;
+}
+
+int Image::image_sz() {
+    return width * height;
+}
+
+
+void Image::copy_fields(const Image& img2) {
+}
+
+
+/*
+ * Setting `display() = 0` here makes this an abstract
+ * class that can't be implemented.
+ * */
+string Image::display(std::string s) {
+    return "Displaying image " + s;
+}
+
+
